5c9310ab562ab53b1b2829f573312ae9a94624bc
EmJeyDev <emjeydev@gmail.com>
2023-02-06 20:18:32 +0330

    add ExponentFunction Priject

---
+++ ExponentFunction/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This file is made by EmJey
+// Project: ExponentFunction
+// FileName: ExponentFunction
+// Date: 2023/02/06
+// Email: emjeydev@gmail.com
+// Github: emjeydev
+
+namespace ExponentFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNum, powNum;
+            Console.Write("Base number: ");
+            baseNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Powwer number: ");
+            powNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(GetPow(baseNum, powNum));
+
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int resutl = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                resutl *= baseNum;
+            }
+
+            return resutl;
+        }
+    }
+}
